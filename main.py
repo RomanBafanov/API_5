a5b1cde6bb9872c62b39e1fea9bb43ddb88058dc
Roman Bafanov <108800547+RomanBafanov@users.noreply.github.com>
2023-11-06 10:52:40 +0300

    main.py bugs fixed

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from dotenv import load_dotenv
 import os
 
 
+AREA = '1'
+PERIOD = '31'
+TOWN = 4
+CATALOGUES = 48
+COUNT = 100
+
+
 def get_response_hh(language):
-    data = []
+    info_about_vacancies_hh = []
     page = 0
     pages_number = 1
     url = 'https://api.hh.ru/vacancies'
     while page < pages_number:
         params = {
             'text': f'NAME:Программист {language}',
-            'area': '1',
-            'period': '31',
+            'area': AREA,
+            'period': PERIOD,
             'page': page,
         }
         page_response = requests.get(url, params=params)
@@ -21,13 +28,13 @@ def get_response_hh(language):
         page_payload = page_response.json()
         pages_number = page_payload['pages']
         page += 1
-        data.append(page_payload)
+        info_about_vacancies_hh.append(page_payload)
 
-    return data, pages_number
+    return info_about_vacancies_hh, pages_number
 
 
 def get_response_sj(language, api_key):
-    data = []
+    info_about_vacancies_sj = []
     page = 0
     url = 'https://api.superjob.ru/2.0/vacancies/'
     headers = {
@@ -35,9 +42,9 @@ def get_response_sj(language, api_key):
     }
     while True:
         params = {
-            'town': 4,
-            'catalogues': 48,
-            'count': 100,
+            'town': TOWN,
+            'catalogues': CATALOGUES,
+            'count': COUNT,
             'keyword': language,
             'page': page
         }
@@ -45,16 +52,16 @@ def get_response_sj(language, api_key):
         response.raise_for_status()
         page_response = response.json()
         total = page_response['total']
-        data.append(page_response)
-        if page_response['more'] == False:
-            return data, total
+        info_about_vacancies_sj.append(page_response)
+        if not page_response['more']:
+            return info_about_vacancies_sj, total
         else:
             page += 1
 
 
 def predict_rub_salary_hh(vacancy):
     vac_salary = vacancy['salary']
-    if vac_salary is not None:
+    if vac_salary:
         if vac_salary['from'] is None:
             salary = int(vac_salary['to'] * 0.8)
             return salary
@@ -82,70 +89,81 @@ def predict_rub_salary_sj(vacancy):
 
 
 def output_console(average_salaries_for_vacancies, website):
-    analyzed_salaries = []
-    analyzed_salaries.append(['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата'])
+    analyzed_salaries = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
     for language, analysis in average_salaries_for_vacancies.items():
-        job_analysis = []
-        job_analysis.append(language)
+        job_analysis = [language]
         for count in analysis.values():
             job_analysis.append(count)
         analyzed_salaries.append(job_analysis)
+
     title = f'{website} Moscow'
     table_instance = AsciiTable(analyzed_salaries, title)
-    print(table_instance.table)
+
+    return table_instance
 
 
 def create_salaries_hh(languages):
     website = 'HeadHunter'
     average_salaries_for_vacancies = {}
     for language in languages:
-        average_salaries_for_vacancies[language] = {}
-        data, pages_number = get_response_hh(language)
+        info_about_vacancies, pages_number = get_response_hh(language)
         salaries = []
-        average_salaries_for_vacancies[language]['vacancies_found'] = pages_number
-        count = 0
-        for vacancies in data:
+        for vacancies in info_about_vacancies:
             for vacancy in vacancies['items']:
                 salary = predict_rub_salary_hh(vacancy)
-                if salary is not None:
-                    count += 1
+                if salary:
                     salaries.append(salary)
-        average_salaries_for_vacancies[language]['vacancies_processed'] = count
-        average = int(sum(salaries) / len(salaries))
-        average_salaries_for_vacancies[language]['average_salary'] = average
+        try:
+            average = int(sum(salaries) / len(salaries))
+        except ZeroDivisionError:
+            print("Cannot divide by zero!")
+        count = len(salaries)
+        average_salaries_for_vacancies[language] = {
+                'vacancies_found': pages_number,
+                'vacancies_processed': count,
+                'average_salary': average,
+        }
 
-    output_console(average_salaries_for_vacancies, website)
+    table = output_console(average_salaries_for_vacancies, website)
+
+    return table
 
 
-def create_salaries_sj(languages):
+def create_salaries_sj(languages, api_key):
     website = 'Super Job'
     average_salaries_for_vacancies = {}
-    load_dotenv()
-    api_key = os.getenv('API_KEY_SJ')
     for language in languages:
-        average_salaries_for_vacancies[language] = {}
-        data, total = get_response_sj(language, api_key)
-        average_salaries_for_vacancies[language]['vacancies_found'] = total
+        info_about_vacancies_sj, total = get_response_sj(language, api_key)
         salaries = []
-        count = 0
-        for d in data:
-            for vacancy in d['objects']:
+        for vacancies in info_about_vacancies_sj:
+            for vacancy in vacancies['objects']:
                 salary = predict_rub_salary_sj(vacancy)
-                if salary is not None:
-                    count += 1
+                if salary:
                     salaries.append(salary)
         if salaries:
-            average_salaries_for_vacancies[language]['vacancies_processed'] = count
-            average = int(sum(salaries) / len(salaries))
-            average_salaries_for_vacancies[language]['average_salary'] = average
+            try:
+                average = int(sum(salaries) / len(salaries))
+            except ZeroDivisionError:
+                print("Cannot divide by zero!")
+            count = len(salaries)
+            average_salaries_for_vacancies[language] = {
+                    'vacancies_found': total,
+                    'vacancies_processed': count,
+                    'average_salary': average,
+            }
 
-    output_console(average_salaries_for_vacancies, website)
+    table = output_console(average_salaries_for_vacancies, website)
+
+    return table
 
 
 def main():
-    programming_language = ['Python', 'Java', 'Javascript', 'Go', 'PHP', 'C++',]
-    create_salaries_hh(programming_language)
-    create_salaries_sj(programming_language)
+    load_dotenv()
+    api_key = os.getenv('API_KEY_SJ')
+    programming_language = ['Python', 'Java', 'Javascript', 'Go', 'PHP', 'C++']
+    table_hh = create_salaries_hh(programming_language)
+    table_sj = create_salaries_sj(programming_language, api_key)
+    print(f"{table_hh.table}\n\n{table_sj.table}")
 
 
 if __name__ == '__main__':
